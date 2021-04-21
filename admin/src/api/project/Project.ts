d132e390af97d456061595ec404d1e155108a23e
Roman Kushyn <romadzao@gmail.com>
2021-04-21 23:43:55 +0300

    amplication-build-cknrxbiqc5601331bs6gtjbmxce

---
+++ admin/src/api/project/Project.ts
@@ -0,0 +1,13 @@
+import { UserWhereUniqueInput } from "../user/UserWhereUniqueInput";
+
+export type Project = {
+  createdAt: Date;
+  description: string | null;
+  dueDate: Date | null;
+  id: string;
+  name: string | null;
+  owner?: UserWhereUniqueInput;
+  startDate: Date | null;
+  updatedAt: Date;
+  whateverField: string | null;
+};
